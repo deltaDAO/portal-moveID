b13098d376e79b29be727304e511c916c4decfd6
Moritz Kirstein <mkirstein@protonmail.com>
2024-03-04 16:34:44 +0100

    chore: move to new usecase route

--- src/pages/usecases/roaddamage.tsx
+++ src/pages/usecases/roaddamage.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from 'react'
 import Page from '@shared/Page'
-import router from 'next/router'
-import content from '../../content/pages/roaddamage.json'
-import RoadDamage from '../components/RoadDamage'
-import AssetProvider from '../@context/Asset'
-import { ROAD_DAMAGE_ALGO_DID } from '../components/RoadDamage/_constants'
+import { useRouter } from 'next/router'
+import content from '../../../content/pages/roaddamage.json'
+import RoadDamage from '../../components/RoadDamage'
+import AssetProvider from '../../@context/Asset'
+import { ROAD_DAMAGE_ALGO_DID } from '../../components/RoadDamage/_constants'
 
 export default function PageRoadDamage(): ReactElement {
+  const router = useRouter()
+
   const { title, description } = content
 
   return (
