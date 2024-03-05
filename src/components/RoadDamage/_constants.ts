7594c754470bac8f6881b06a48a173543cd26f79
Moritz Kirstein <mkirstein@protonmail.com>
2024-03-05 16:29:24 +0100

    chore: remove mock

--- src/components/RoadDamage/_constants.ts
+++ src/components/RoadDamage/_constants.ts
@@ -1,8 +1,7 @@
 import { RoadDamage } from './_types'
 
 export const ROAD_DAMAGE_ALGO_DID =
-  'did:op:77ff2a1f7ecb35b2684ecdd8f932bfdc8a66421345fc4e056f262ef627d70e15' // GEN-X
-// 'did:op:a1f800fadd32267ae6662b4e411396bd180242ca8274810c9e1a771f3920468d' // PONTUS-X
+  'did:op:926098d058b017dcf3736370f3c3d77e6046ca6622af111229accf5f9c83e308'
 
 export const ROAD_DAMAGE_USECASE_NAME = 'roaddamage'
 
@@ -33,30 +32,5 @@ export const CONFIDENCE_COLOR_MAP = [
   {
     threshold: 0,
     color: 'grey'
-  }
-]
-
-const mapDataMock: RoadDamage[] = [
-  {
-    type: 'crack',
-    damageClass: 'D10',
-    lastObservation: '2001-10-26T21:32:52', // timestamp of image taken
-    heading: 'string',
-    gpsCoordinates: {
-      lat: 12.1234,
-      lng: 12.1234
-    },
-    confidence: 0.23
-  },
-  {
-    type: 'pothole',
-    damageClass: 'D10',
-    lastObservation: '2001-10-26T21:32:52', // timestamp of image taken
-    heading: 'string',
-    gpsCoordinates: {
-      lat: 12.5678,
-      lng: 12.5678
-    },
-    confidence: 0.92
   }
 ]
